f7b796755e8d1a7417cc456f7202967214dc5c11
evaldmonastyrski <evaldmon@gmail.com>
2018-09-16 23:08:31 +0300

    evaldmon: Initial Commit

--- Formula One Game/Program.cs
+++ Formula One Game/Program.cs
@@ -17,6 +17,7 @@ namespace Formula_One_Game
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Hello
         }
     }
 }
