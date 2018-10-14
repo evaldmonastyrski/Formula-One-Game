4596fb37789bf733fda5b6d877c7ee1e8055f383
evaldmonastyrski <evaldmon@gmail.com>
2018-10-14 14:28:46 +0300

    evaldmon: Rename private fields

--- Formula One Game/Team Components/Team.cs
+++ Formula One Game/Team Components/Team.cs
@@ -9,17 +9,17 @@ namespace Formula_One_Game
     class Team : DreamTeamComponent, IComparable
     {
         public string Name { get; }
-        private List<Driver> Drivers;
+        private List<Driver> drivers;
 
         public Team(string name)
         {
             Name = name;
-            Drivers = new List<Driver>();
+            drivers = new List<Driver>();
         }
 
         public void AddDriver(Driver driver)
         {
-            Drivers.Add(driver);
+            drivers.Add(driver);
         }
 
         public int CompareTo(object obj)
