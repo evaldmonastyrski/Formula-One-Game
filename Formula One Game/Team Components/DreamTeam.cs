df1352c5de4795bc196aab154b6f9b483dcd5331
evelinamon <43343565+evelinamon@users.noreply.github.com>
2018-10-18 13:42:28 +0300

    evelinamon: add prices till Austria GP, change drivers place, change Form1.Designer, add driver price, sort by price

--- Formula One Game/Team Components/DreamTeam.cs
+++ Formula One Game/Team Components/DreamTeam.cs
@@ -12,6 +12,7 @@ namespace Formula_One_Game
         private Driver Driver2;
         private Team Team;
         private Engine Engine;
+        public float Price;
 
         public DreamTeam(Driver driver1, Driver driver2, Team team, Engine engine)
         {
@@ -19,6 +20,7 @@ namespace Formula_One_Game
             Driver2 = driver2;
             Team = team;
             Engine = engine;
+            Price = calculatePrice();
         }
 
         public string GetDriver1Surname()
@@ -39,6 +41,11 @@ namespace Formula_One_Game
         public string GetEngineName()
         {
             return Engine.Name;
+        }
+
+        public float calculatePrice()
+        {
+            return 0F;
         }
 
         public int CompareTo(object obj)
