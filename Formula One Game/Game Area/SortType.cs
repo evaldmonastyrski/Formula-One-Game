7836ceb3477bf77524c16c7da564218b26118e75
evaldmonastyrski <evaldmon@gmail.com>
2018-11-10 15:28:42 +0200

    evaldmon: ammend the previous commit

---
+++ Formula One Game/Game Area/SortType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula_One_Game
+{
+    enum SortType
+    {
+        POINTS,
+        PRICE_CHANGE,
+    }
+}
